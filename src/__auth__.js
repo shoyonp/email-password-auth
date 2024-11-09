a0f5ba275fe5ec455107c99184719e8bed7ee113
shoyonp <shoyonkumar143@gmail.com>
2024-11-10 01:20:31 +0600

    set up done navbar added

---
+++ src/__auth__.js
@@ -0,0 +1,5 @@
+/**
+ * 1. login === sign in => you are an old user . you already have the credential (email and password) to login.
+ * 2. register === sign up === creat account ===> you are a brand new user. you have never used the website before. you do not have any email and password that have access 
+ * 
+ */
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-4xl">welcome to email password home </h2>
+        </div>
+    );
+};
+
+export default Home;
